a51a939ed3aac3129a9970827a25a25f9a09a828
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-01-20 20:08:59 +0600

    Fetching BlogPost using /getblog Endpoint.

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import styles from '../../styles/BlogPost.module.css'
 
 const Slug = () => {
+  const [blog, setBlog] = useState([]);
   const router = useRouter()
+  useEffect(() => {
+    
+    if(!router.isReady) return;
   const { slug } = router.query
+  
+    fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((parsed) => {
+       // console.log(parsed);
+        setBlog(parsed);
+      });
+  }, [router.isReady]);
 
   return <div className={styles.container}>
-    <div className={styles.main}>
-    <h1>Title About {slug}</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora qui non deserunt. 
-      Tempore fugit ratione necessitatibus, soluta similique quisquam? 
-      Hic explicabo inventore quisquam dolores cupiditate rerum maiores quo sunt error voluptatum dolor tempore sapiente similique, 
-      quam, modi aliquam sint laborum, distinctio id! Voluptatum, accusamus illo!</p>
+    <div className={styles.main} key={blog.slug}>
+    <h1> {blog && blog.title}</h1>
+    <p>{blog && blog.content}</p>
     </div>
     </div>
 }
