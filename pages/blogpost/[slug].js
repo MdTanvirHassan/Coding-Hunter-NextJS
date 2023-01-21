4099b52c77aaf0038a1d9f5da57e9b39f6291245
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-01-21 23:47:25 +0600

    Server Side Rendering

--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -2,23 +2,23 @@ import React, {useState, useEffect} from 'react'
 import { useRouter } from 'next/router'
 import styles from '../../styles/BlogPost.module.css'
 
-const Slug = () => {
-  const [blog, setBlog] = useState([]);
-  const router = useRouter()
-  useEffect(() => {
+const Slug = (props) => {
+  const [blog, setBlog] = useState(props.myBlog);
+  // const router = useRouter()
+  // useEffect(() => {
     
-    if(!router.isReady) return;
-  const { slug } = router.query
+  //   if(!router.isReady) return;
+  // const { slug } = router.query
   
-    fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
-      .then((response) => {
-        return response.json();
-      })
-      .then((parsed) => {
-       // console.log(parsed);
-        setBlog(parsed);
-      });
-  }, [router.isReady]);
+  //   fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((parsed) => {
+  //      // console.log(parsed);
+  //       setBlog(parsed);
+  //     });
+  // }, [router.isReady]);
 
   return <div className={styles.container}>
     <div className={styles.main} key={blog.slug}>
@@ -26,6 +26,16 @@ const Slug = () => {
     <p>{blog && blog.content}</p>
     </div>
     </div>
+};
+
+export async function getServerSideProps(context) {
+  const {slug} = context.query;
+  let data = await fetch(`http://localhost:3000/api/getblog?slug=${slug}`)
+  let myBlog = await data.json();
+      
+  return {
+    props: {myBlog}, 
+  }
 }
 
 export default Slug
